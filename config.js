9eba04febd99cdff2fcd47a282574af67e372613
brads4z <brads4z@gmail.com>
2019-10-23 17:04:56 -0400

    Revert "Delete config.js"
    This reverts commit d937b1704e3b8873ad5e1d26c6a44ef5f52aa730.

---
+++ config.js
@@ -0,0 +1,16 @@
+const db = {
+    CONNECTION_URL: "mongodb+srv://admin:RkOGIK3IYKH01YeK@my-nibbl-db-72g5b.mongodb.net/nibbl?retryWrites=true&w=majority",
+    DATABASE_NAME: "nibbl_db"
+};
+
+const keys = {
+    SALT: 'bcdgirualskd544585afsa32fa54fa1s4f5asdfnnvzxlij5869'
+};
+
+const rootChunkid = '5da5f3e0ebcdb4280ce85eb6';
+
+module.exports = {
+    db,
+    keys,
+    rootChunkid
+}; 
